d218ef6915e85c53b2b9999e8cb90cd415ba99cf
Javier-H3 <hernandezgomezjavier369@gmail.com>
2026-06-23 18:51:17 -0500

    refactor(cambios en los comentarios del codigo): Cambie los comentarios del codigo a español para mejorar el entendimiento del codigo y adicionalmente los segmentos de codigo comentado los cambie a ''' para manejarlos mas facil.
    Refs #5

--- controllers/crazyfliepy/crazyfliepy.py
+++ controllers/crazyfliepy/crazyfliepy.py
@@ -1,50 +1,26 @@
-# Copyright 2022 Bitcraze AB
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# ...........       ____  _ __
-# |  ,-^-,  |      / __ )(_) /_______________ _____  ___
-# | (  O  ) |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
-# | / ,..´  |    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
-#    +.......   /_____/_/\__/\___/_/   \__,_/ /___/\___/
-#
-# @file crazyflie_controller.py
-# Description: Controls the crazyflie in webots
-# Author:      Kimberly McGuire (Bitcraze AB)
-
 import math
 from controller import Robot, Camera, DistanceSensor, GPS, Gyro, InertialUnit, Keyboard, Motor
 import scipy.interpolate as spi
 
-# Add external controller
+# Añadir el controlador externo - pid_controller.py
 from pid_controller import (
-    ActualState,
-    DesiredState,
-    GainsPID,
-    MotorPower,
+    ActualState, #Estado actual
+    DesiredState, #Estado deseado
+    GainsPID, # Ganancias del PID
+    MotorPower, #Potencia del motor
     init_pid_attitude_fixed_height_controller,
     pid_velocity_fixed_height_controller,
 )
 
-FLYING_ALTITUDE = 1.0
-
+FLYING_ALTITUDE = 1.0 #Estable la altura fija del drone - 1m
 
 def main():
+
     robot = Robot()
 
     timestep = int(robot.getBasicTimeStep())
-
-    # Initialize motors
+    
+    #Inicia los motores
     m1_motor = robot.getDevice("m1_motor")
     m1_motor.setPosition(float("inf"))
     m1_motor.setVelocity(-1.0)
@@ -61,22 +37,23 @@ def main():
     m4_motor.setPosition(float("inf"))
     m4_motor.setVelocity(1.0)
 
-    # Initialize sensors
+    # Inicializar los sensores
     imu = robot.getDevice("inertial_unit")
     imu.enable(timestep)
 
     gps = robot.getDevice("gps")
     gps.enable(timestep)
 
-    keyboard = robot.getKeyboard()
+    keyboard = robot.getKeyboard() #Debido a que    
     keyboard.enable(timestep)
 
     gyro = robot.getDevice("gyro")
     gyro.enable(timestep)
 
-    camera = robot.getDevice("camera")
+    camera = robot.getDevice("camera") # El que me interesa por ahora
     camera.enable(timestep)
 
+    # Permiten conocer las distancias de los obstaculos
     range_front = robot.getDevice("range_front")
     range_front.enable(timestep)
 
@@ -88,20 +65,21 @@ def main():
 
     range_right = robot.getDevice("range_right")
     range_right.enable(timestep)
+    ###################################################
 
-    # Wait for 2 seconds
+    # Espera por dos segundos - El tiempo es necesario para estabilizar la simulacion y los sensores.
     while robot.step(timestep) != -1:
         if robot.getTime() > 2.0:
             break
 
-    # Initialize variables
+    # Iniciacion de las variables 
     actual_state = ActualState()
     desired_state = DesiredState()
     past_x_global = 0.0
     past_y_global = 0.0
     past_time = robot.getTime()
 
-    # Initialize PID gains
+    # Iniciacion de los valores (gains) para el controlador PID
     gains_pid = GainsPID()
     gains_pid.kp_att_y = 1.0
     gains_pid.kd_att_y = 0.5
@@ -116,7 +94,7 @@ def main():
 
     height_desired = FLYING_ALTITUDE
 
-    # Initialize struct for motor power
+    # Inicializar la estructura para la potencia del motor
     motor_power = MotorPower()
 
     print()
@@ -140,7 +118,7 @@ def main():
     while robot.step(timestep) != -1:
         dt = robot.getTime() - past_time
 
-        # Get measurements
+        # Obtiene las mediciones
         rpy = imu.getRollPitchYaw()
         actual_state.roll = rpy[0]
         actual_state.pitch = rpy[1]
@@ -152,14 +130,14 @@ def main():
         y_global = gps.getValues()[1]
         vy_global = (y_global - past_y_global) / dt
 
-        # Get body fixed velocities
+        # Obtener velocidades fijas del cuerpo
         actual_yaw = imu.getRollPitchYaw()[2]
         cos_yaw = math.cos(actual_yaw)
         sin_yaw = math.sin(actual_yaw)
         actual_state.vx = vx_global * cos_yaw + vy_global * sin_yaw
         actual_state.vy = -vx_global * sin_yaw + vy_global * cos_yaw
 
-        # Initialize desired state values
+        # Iniciacion los valores de estado deseados
         desired_state.roll = 0.0
         desired_state.pitch = 0.0
         desired_state.vx = 0.0
@@ -172,51 +150,54 @@ def main():
         yaw_desired = 0.0
         height_diff_desired = 0.0
 
-        # Read keyboard input
-        #key = keyboard.getKey()
-        #while key > 0:
-        #    if key == Keyboard.UP:
-        #        forward_desired = +0.5
-        #    elif key == Keyboard.DOWN:
-        #        forward_desired = -0.5
-        #    elif key == Keyboard.RIGHT:
-        #        sideways_desired = -0.5
-        #    elif key == Keyboard.LEFT:
-        #        sideways_desired = +0.5
-        #    elif key == ord("Q"):
-        #        yaw_desired = 1.0
-        #    elif key == ord("E"):
-        #        yaw_desired = -1.0
-        #    elif key == ord("W"):
-        #        height_diff_desired = 0.1
-        #    elif key == ord("S"):
-        #        height_diff_desired = -0.1
+        '''# Control del drone mediante el teclado
+        key = keyboard.getKey()
+        while key > 0:
+            if key == Keyboard.UP:
+                forward_desired = +0.5
+            elif key == Keyboard.DOWN:
+                forward_desired = -0.5
+            elif key == Keyboard.RIGHT:
+                sideways_desired = -0.5
+            elif key == Keyboard.LEFT:
+                sideways_desired = +0.5
+            elif key == ord("Q"):
+                yaw_desired = 1.0
+            elif key == ord("E"):
+                yaw_desired = -1.0
+            elif key == ord("W"):
+                height_diff_desired = 0.1
+            elif key == ord("S"):
+                height_diff_desired = -0.1
 
-        #    key = keyboard.getKey()
+            key = keyboard.getKey()
         tcurr = robot.getTime()-tini
-
-        ## Movimientos a velocidad constante
-        #theta = 2*math.pi*tcurr/10.0
-        #v = 0.5
-        #forward_desired = v*math.cos(theta)
-        #sideways_desired = v*math.sin(theta)
-        #if tcurr > 5:
-        #    height_diff_desired = 0.15*math.sin(2*math.pi*3*tcurr)
-        #yaw_desired = 0.0
-
-        ## Controlador tipo robot DDR
-        # Kv = 0.5
-        # Kh = 1.0
-        # xd = 0
-        # yd = 0
-        # errp = math.sqrt((x_global-xd)**2 + (y_global-yd)**2)
-        # ct = math.cos(rpy[2])
-        # st = math.sin(rpy[2])
-        # errh = math.atan2(ct*(yd-y_global)-st*(xd-x_global), st*(yd-y_global)+ct*(xd-x_global))
-        # forward_desired = Kv*errp
-        # yaw_desired = Kh*errh
+        '''
         
-        # Controlador para la orientación o yaw
+        '''## Movimientos a velocidad constante
+        theta = 2*math.pi*tcurr/10.0
+        v = 0.5
+        forward_desired = v*math.cos(theta)
+        sideways_desired = v*math.sin(theta)
+        if tcurr > 5:
+            height_diff_desired = 0.15*math.sin(2*math.pi*3*tcurr)
+        yaw_desired = 0.0
+        '''
+        
+        '''## Controlador tipo robot DDR
+         Kv = 0.5
+         Kh = 1.0
+         xd = 0
+         yd = 0
+         errp = math.sqrt((x_global-xd)**2 + (y_global-yd)**2)
+         ct = math.cos(rpy[2])
+         st = math.sin(rpy[2])
+         errh = math.atan2(ct*(yd-y_global)-st*(xd-x_global), st*(yd-y_global)+ct*(xd-x_global))
+         forward_desired = Kv*errp
+         yaw_desired = Kh*errh
+        '''
+
+        # Controlador para la orientación o yaw - Segmento que se encarga de la navegacion automatica
         Kh = 5.0
         th = rpy[2]
 
@@ -250,25 +231,25 @@ def main():
         else:
             height_desired += height_diff_desired * dt
 
-        # Example of how to get sensor data:
+        # Ejemplo de cómo obtener datos de los sensores:
         # range_front_value = range_front.getValue()
         # image = camera.getImage()
 
         desired_state.yaw_rate = yaw_desired
 
-        # PID velocity controller with fixed height
+        # Controlador de velocidad PID con altura fija
         desired_state.vy = sideways_desired
         desired_state.vx = forward_desired
         desired_state.altitude = height_desired
         pid_velocity_fixed_height_controller(actual_state, desired_state, gains_pid, dt, motor_power)
 
-        # Set motor speeds
+        # Establecer o configurar las velocidades de los motores
         m1_motor.setVelocity(-motor_power.m1)
         m2_motor.setVelocity(motor_power.m2)
         m3_motor.setVelocity(-motor_power.m3)
         m4_motor.setVelocity(motor_power.m4)
 
-        # Save past values for next time step
+        # Guardar los valores anteriores para el siguiente paso temporal
         past_time = robot.getTime()
         past_x_global = x_global
         past_y_global = y_global
